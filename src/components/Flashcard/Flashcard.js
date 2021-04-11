be339e6eb9cc96eaaa35de63e4d2bf7e97444393
tnobile <takao.nosaka@outlook.com>
2021-04-12 00:17:10 +0200

    css module

--- src/components/Flashcard/Flashcard.js
+++ src/components/Flashcard/Flashcard.js
@@ -1,12 +1,23 @@
 import React, { useState } from 'react'
+import classes from "./Flashcard.module.css"
 
 const Flashcard = (props) => {
     const [flip, setFlip] = useState(false);
 
+    const f = flip ? 'flip' : '';
     return (
-        <div onClick={() => setFlip(!flip)}>
-            { flip ? props.flashcard.answer : props.flashcard.question}
-        </div>
+        <div className={[classes.card, classes[f]].join(",")}
+            onClick={() => setFlip(!flip)}>
+            <div classname={classes.front}>
+                {props.flashcard.question}
+                <div className={classes['flashcard-options']}>
+                    {props.flashcard.options.map(o => {
+                        return <div className={classes['flashcard-option']}>{o}</div>
+                    })}
+                </div>
+            </div>
+            <div className={classes.back}>{props.flashcard.answer}</div>
+        </div >
     )
 }
 
